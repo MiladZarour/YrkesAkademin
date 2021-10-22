1037d0c6e1d64e9281ec79563fa22850bc4c0d1c
BorisBug <69357420+BorisBug@users.noreply.github.com>
2021-10-22 20:30:39 +0200

    more examples ex-22

--- SV21POTR/Exercises/exercise-21/exercise-21.c
+++ SV21POTR/Exercises/exercise-21/exercise-21.c
@@ -9,21 +9,24 @@ numbers in the range of 0 and 99 and then print the array to the terminal.
 To fill and print the array, use only pointers.
 */
 
-#define SIZE1 2 // rows
-#define SIZE2 3 // columns
+#define SIZE1 4 // rows
+#define SIZE2 7 // columns
 
 int main()
 {
     int arr[SIZE1][SIZE2];
+    int *ptr=(int*)arr;
     
     // randomize
     srand(time(NULL));
     for (int i=0; i<SIZE1; i++)
     for (int j=0; j<SIZE2; j++)
         *(*(arr+i)+j) = rand() % 100;
-    
+        //arr[i][j] = rand() % 100;
+        //*(ptr+i*SIZE2+j) = rand() % 100;
+
     // print using pointers
-    printf("Matrix (using **):\n");
+    printf("\nA) Matrix (using **) on arr:\n");
     for (int i=0; i<SIZE1; i++)
     {
         for (int j=0; j<SIZE2; j++)
@@ -32,9 +35,18 @@ int main()
         printf("\n");
     }
 
-    // print using index to TEST that the answer is correct and equivalent
-    printf("Matrix (using [][]):\n");
+    // print using pointers
+    printf("\nB) Matrix (using *) on ptr:\n");
+    for (int i=0; i<SIZE1; i++)
+    {
+        for (int j=0; j<SIZE2; j++)
+            printf("%4d", *(ptr+i*SIZE2+j));
 
+        printf("\n");
+    }
+
+    // print using index to TEST that the answer is correct and equivalent
+    printf("\nC) Matrix (using [][] on arr):\n");
     for (int i=0; i<SIZE1; i++)
     {
         for (int j=0; j<SIZE2; j++)
